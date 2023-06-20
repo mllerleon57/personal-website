700173ca41863d159c09d9899ea44711915fc71c
Leon <43845277+mllerleon57@users.noreply.github.com>
2023-06-20 10:32:54 +0000

    done

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,13 +1,17 @@
 sap.ui.define(
-    ["./BaseController"],
+    ["./BaseController", "sap/m/library"],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, sapMLib) {
         "use strict";
 
         return Controller.extend("leon.cv.personalcv.controller.MainView", {
-            onInit: function () {},
+            openUrl: function (urlPart, newTab) {
+                const url = urlPart
+                const { URLHelper } = sapMLib; // sapMLib required from "sap/m/library"
+                URLHelper.redirect(url, newTab);
+            },
         });
     }
 );
